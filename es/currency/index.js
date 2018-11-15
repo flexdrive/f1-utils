4a8a41643161f3fd11c31210f8a79fa0df7f84e3
jason-flexdrive <jason.steele@flexdrive.com>
2018-11-15 11:10:25 -0500

    need the built versions

---
+++ es/currency/index.js
@@ -0,0 +1,87 @@
+var numeral = require('numeral');
+
+var symbols = {
+  'NOK': {
+    delimiters: {
+      thousands: ' ',
+      decimal: ','
+    },
+    currency: {
+      symbol: 'kr',
+      symbolPosition: 'POST'
+    },
+    format: '0,0.00 $',
+    raw: '0,0.00'
+  },
+  'USD': {
+    delimiters: {
+      thousands: ',',
+      decimal: '.'
+    },
+    currency: {
+      symbol: '$',
+      symbolPosition: 'PRE'
+    },
+    format: '$0,0.00',
+    raw: '0,0.00'
+  }
+};
+export function setLocale() {
+  var locale = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'USD';
+  var localeSettings = symbols[locale.toUpperCase()];
+  numeral.register('locale', locale, localeSettings);
+  numeral.locale(locale);
+
+  var format = function format(val) {
+    var raw = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+    var dollars = centsToDollars(val);
+    return numeral(parseFloat(dollars)).format(raw ? localeSettings.raw : localeSettings.format);
+  };
+
+  var currencySymbol = function currencySymbol() {
+    return localeSettings.currency.symbol;
+  };
+
+  var currencySymbolPosition = function currencySymbolPosition() {
+    return numeral.localeData().currency.symbolPosition;
+  };
+
+  var localeDecimal = function localeDecimal() {
+    return numeral.localeData().delimiters.decimal;
+  };
+
+  var centsToDollars = function centsToDollars() {
+    var amt = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+    var decimals = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
+    var value = amt;
+
+    if (typeof amt === 'string') {
+      value = parseInt(value);
+    }
+
+    if (value === 0) {
+      return "".concat(value);
+    }
+
+    if (decimals === 0) {
+      return "".concat(value / 100);
+    }
+
+    return (value / 100).toFixed(decimals);
+  };
+
+  var dollarsToCents = function dollarsToCents(amt) {
+    var value = typeof amt === 'string' ? parseFloat(amt.replace(localeDecimal(), '.')) : amt;
+    return Math.round(100 * value);
+  };
+
+  var methods = {
+    format: format,
+    currencySymbol: currencySymbol,
+    currencySymbolPosition: currencySymbolPosition,
+    localeDecimal: localeDecimal,
+    centsToDollars: centsToDollars,
+    dollarsToCents: dollarsToCents
+  };
+  return methods;
+}
