8666f534d0fd047a6d132cdd1d657fdc88edfa8e
Adrian Santos <adrian.santos@gruposantander.com>
2021-01-13 11:29:24 +0100

    With changes in the package

--- dist/index.js
+++ dist/index.js
@@ -1226,7 +1226,27 @@
 	  ERROR_ACTIVITY_ACTION_NOT_EXIST: 'ERROR_ACTIVITY_ACTION_NOT_EXIST',
 	  ERROR_INVALID_USERNAME_PASSWORD: 'ERROR_INVALID_USERNAME_PASSWORD',
 	  ERROR_INVALID_TOKEN: 'ERROR_INVALID_TOKEN',
-	  ERROR_PASSWORD_EXPIRED: 'ERROR_PASSWORD_EXPIRED'
+	  ERROR_PASSWORD_EXPIRED: 'ERROR_PASSWORD_EXPIRED',
+	  ERROR_MOBILE_SERVICE_LOGIN_REQUIRED: 'ERROR_MOBILE_SERVICE_LOGIN_REQUIRED',
+	  ERROR_MOBILE_SERVICE_LOGALTY_SERVICE_URL: 'ERROR_MOBILE_SERVICE_LOGALTY_SERVICE_URL',
+	  ERROR_MOBILE_SERVICE_LOOKS_LIKE: 'ERROR_MOBILE_SERVICE_LOOKS_LIKE',
+	  ERROR_MOBILE_SERVICE_INVALID_INPUT: 'ERROR_MOBILE_SERVICE_INVALID_INPUT',
+	  ERROR_MOBILE_SERVICE_PLEASE_ENTER_EMAIL: 'ERROR_MOBILE_SERVICE_PLEASE_ENTER_EMAIL',
+	  ERROR_INVALID_CODE: 'ERROR_INVALID_CODE',
+	  ERROR_PASSWORD_PREVIOUSLY_USED: 'ERROR_PASSWORD_PREVIOUSLY_USED',
+	  ERROR_MOBILE_SERVICE_UNKNOWN_ERROR: 'ERROR_MOBILE_SERVICE_UNKNOWN_ERROR'
+	};
+
+	var Messages = {
+	  MSG_MOBILE_TITLE_ROADSIDE: 'MSG_MOBILE_TITLE_ROADSIDE',
+	  MSG_MOBILE_DESCRIPTION_ROADSIDE: 'MSG_MOBILE_DESCRIPTION_ROADSIDE',
+	  MSG_MOBILE_LEARN_MORE: 'MSG_MOBILE_LEARN_MORE',
+	  MSG_MOBILE_TITLE_INSURANCE: 'MSG_MOBILE_TITLE_INSURANCE',
+	  MSG_MOBILE_DESCRIPTION_INSURANCE: 'MSG_MOBILE_DESCRIPTION_INSURANCE',
+	  MSG_MOBILE_TITLE_MAINTENANCE: 'MSG_MOBILE_TITLE_MAINTENANCE',
+	  MSG_MOBILE_DESCRIPTION_MAINTENANCE: 'MSG_MOBILE_DESCRIPTION_MAINTENANCE',
+	  MSG_MOBILE_TITLE_DRIVESY: 'MSG_MOBILE_TITLE_DRIVESY',
+	  MSG_MOBILE_DESCRIPTION_DRIVESY: 'MSG_MOBILE_DESCRIPTION_DRIVESY'
 	};
 
 	var DEFAULT_LANGUAGE_CODE = 'en-US';
@@ -3329,11 +3349,11 @@
 	 * _.keysIn(new Foo);
 	 * // => ['a', 'b', 'c'] (iteration order is not guaranteed)
 	 */
-	function keysIn$1(object) {
+	function keysIn(object) {
 	  return isArrayLike_1(object) ? _arrayLikeKeys(object, true) : _baseKeysIn(object);
 	}
 
-	var keysIn_1 = keysIn$1;
+	var keysIn_1 = keysIn;
 
 	/**
 	 * The base implementation of `_.assignIn` without support for multiple sources
@@ -4112,7 +4132,7 @@
 
 	  var keysFunc = isFull
 	    ? (isFlat ? _getAllKeysIn : _getAllKeys)
-	    : (isFlat ? keysIn : keys_1);
+	    : (isFlat ? keysIn_1 : keys_1);
 
 	  var props = isArr ? undefined : keysFunc(value);
 	  _arrayEach(props || value, function(subValue, key) {
@@ -5464,10 +5484,11 @@
 	  if (arrLength != othLength && !(isPartial && othLength > arrLength)) {
 	    return false;
 	  }
-	  // Assume cyclic values are equal.
-	  var stacked = stack.get(array);
-	  if (stacked && stack.get(other)) {
-	    return stacked == other;
+	  // Check that cyclic values are equal.
+	  var arrStacked = stack.get(array);
+	  var othStacked = stack.get(other);
+	  if (arrStacked && othStacked) {
+	    return arrStacked == other && othStacked == array;
 	  }
 	  var index = -1,
 	      result = true,
@@ -5664,10 +5685,11 @@
 	      return false;
 	    }
 	  }
-	  // Assume cyclic values are equal.
-	  var stacked = stack.get(object);
-	  if (stacked && stack.get(other)) {
-	    return stacked == other;
+	  // Check that cyclic values are equal.
+	  var objStacked = stack.get(object);
+	  var othStacked = stack.get(other);
+	  if (objStacked && othStacked) {
+	    return objStacked == other && othStacked == object;
 	  }
 	  var result = true;
 	  stack.set(object, other);
@@ -9421,6 +9443,7 @@
 
 	exports.Currency = index;
 	exports.Errors = Errors;
+	exports.Messages = Messages;
 	exports.LanguageConfig = index$1;
 	exports.AddressValidation = address;
 
--- es/index.js
+++ es/index.js
@@ -1,7 +1,9 @@
 import * as Currency from './currency';
 import Errors from './error-messages';
+import Messages from './messages';
 import * as LanguageConfig from './languages';
 export { Currency };
 export { AddressValidation } from './validation';
 export { Errors };
+export { Messages };
 export { LanguageConfig };
