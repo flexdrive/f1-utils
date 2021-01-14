479b123b1dedb10eeb5f72286405f2a4f390276c
Adrian Santos <adrian.santos@gruposantander.com>
2021-01-14 11:53:30 +0100

    Without messages

--- es/index.js
+++ es/index.js
@@ -1,9 +1,7 @@
 import * as Currency from './currency';
 import Errors from './error-messages';
-import Messages from './messages';
 import * as LanguageConfig from './languages';
 export { Currency };
 export { AddressValidation } from './validation';
 export { Errors };
-export { Messages };
 export { LanguageConfig };
