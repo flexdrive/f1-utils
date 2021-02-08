5580fae652c636a309d7b1bb655a8361d9a0ad9b
Adrian Santos <adrian.santos@gruposantander.com>
2021-02-08 17:14:39 +0100

    Refactored

--- src/languages/index.js
+++ src/languages/index.js
@@ -3,31 +3,5 @@ const PREFERRED_LANGUAGE = '2'
 const DISABLED_LANGUAGE = '0'
 const LANGUAGES = 3
 const LANGUAGES_SETTINGTYPE = LANGUAGES
-const languages = [
-  {
-    settingKey: 1,
-    settingType: LANGUAGES,
-    languageCode: 'en-US',
-    languageName: 'english'
-  },
-  {
-    settingKey: 2,
-    settingType: LANGUAGES,
-    languageCode: 'es',
-    languageName: 'spanish'
-  },
-  {
-    settingKey: 3,
-    settingType: LANGUAGES,
-    languageCode: 'no',
-    languageName: 'norwegian'
-  }
-]
 
-export {
-  languages,
-  DEFAULT_LANGUAGE_CODE,
-  PREFERRED_LANGUAGE,
-  DISABLED_LANGUAGE,
-  LANGUAGES_SETTINGTYPE
-}
+export { DEFAULT_LANGUAGE_CODE, PREFERRED_LANGUAGE, DISABLED_LANGUAGE, LANGUAGES_SETTINGTYPE }
