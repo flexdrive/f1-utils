2d2686505966bb4ebb1c47df914770e877fd6f34
Adrian Santos <adrian.santos@gruposantander.com>
2021-02-08 17:42:15 +0100

    Refactored

--- dist/index.js
+++ dist/index.js
@@ -1314,25 +1314,8 @@
 	var DISABLED_LANGUAGE = '0';
 	var LANGUAGES = 3;
 	var LANGUAGES_SETTINGTYPE = LANGUAGES;
-	var languages = [{
-	  settingKey: 1,
-	  settingType: LANGUAGES,
-	  languageCode: 'en-US',
-	  languageName: 'english'
-	}, {
-	  settingKey: 2,
-	  settingType: LANGUAGES,
-	  languageCode: 'es',
-	  languageName: 'spanish'
-	}, {
-	  settingKey: 3,
-	  settingType: LANGUAGES,
-	  languageCode: 'no',
-	  languageName: 'norwegian'
-	}];
 
 	var index$1 = /*#__PURE__*/Object.freeze({
-		languages: languages,
 		DEFAULT_LANGUAGE_CODE: DEFAULT_LANGUAGE_CODE,
 		PREFERRED_LANGUAGE: PREFERRED_LANGUAGE,
 		DISABLED_LANGUAGE: DISABLED_LANGUAGE,
--- es/languages/index.js
+++ es/languages/index.js
@@ -3,20 +3,4 @@ var PREFERRED_LANGUAGE = '2';
 var DISABLED_LANGUAGE = '0';
 var LANGUAGES = 3;
 var LANGUAGES_SETTINGTYPE = LANGUAGES;
-var languages = [{
-  settingKey: 1,
-  settingType: LANGUAGES,
-  languageCode: 'en-US',
-  languageName: 'english'
-}, {
-  settingKey: 2,
-  settingType: LANGUAGES,
-  languageCode: 'es',
-  languageName: 'spanish'
-}, {
-  settingKey: 3,
-  settingType: LANGUAGES,
-  languageCode: 'no',
-  languageName: 'norwegian'
-}];
-export { languages, DEFAULT_LANGUAGE_CODE, PREFERRED_LANGUAGE, DISABLED_LANGUAGE, LANGUAGES_SETTINGTYPE };
+export { DEFAULT_LANGUAGE_CODE, PREFERRED_LANGUAGE, DISABLED_LANGUAGE, LANGUAGES_SETTINGTYPE };
